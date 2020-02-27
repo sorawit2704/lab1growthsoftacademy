05fdbe654a566b19cafbe076589578e0c25f32db
sorawit2704 <61543306+sorawit2704@users.noreply.github.com>
2020-02-27 19:14:43 +0700

    mos

--- server.js
+++ server.js
@@ -6,9 +6,80 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const config = require("./config/config.js");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var sessionStore = new MongoDBStore({
+  uri: config.dbUrl,
+  collection: "sessionStore"
+});
+
+const mongooseOption = {
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  reconnectTries: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+};
+
+mongoose.connect(config.dbUrl, mongooseOption, function(err) {
+  if (err) {
+    console.log(
+      "********************************!!! WARNING plzzz !!!*********************************"
+    );
+    console.log(
+      "                          Can't connect to Database. naka e dok"
+    );
+    console.log(
+      "             Please Start database first than restarting this server."
+    );
+    console.log(
+      "**************************************************************************************"
+    );
+
+    console.log(err);
+  } else {
+    console.log(
+      "======================== DB is CONNECTED ========================="
+    );
+  }
+});
+
+app.use(
+  bodyParser.json({
+    limit: "1000mb",
+    parameterLimit: 300000
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    limit: "1000mb",
+    extended: true,
+    parameterLimit: 300000
+  })
+);
+
+// ROUTES FOR OUR API
+// =============================================================================
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+}); 
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -38,6 +109,18 @@ app.delete("/foods/:id", (req, res) => {
   res.status(204).send();
 });
 
+// START THE SERVER
+// =============================================================================
+var http = require("http");
+var port = process.env.PORT || config.httpport;
+var httpServer = http.createServer(app);
+
+httpServer.listen(port, function() {
+  console.log(
+    "=========== HTTP Server started @port ${port} successfully =========="
+  );
+});
+
 app.listen(3000, () => {
-  console.log("Start server at port 3000.");
+  console.log("Start server at port 5713.");
 });
