6e3805a99ef3e2e34cac5808fbac0fcf53539284
sorawit2704 <61543306+sorawit2704@users.noreply.github.com>
2020-03-12 17:30:58 +0700

    .

--- app/routes/shop_route.js
+++ app/routes/shop_route.js
@@ -10,7 +10,7 @@ router.post("/shop/", function(request, response) {
   
     let shop = new Shop(); // กำหนด food ให้เหมือน Schema รอบที่แล้ว
     console.log(shop);
-    shop.name = request.body.shopName; 
+    shop.shopname = request.body.shopName; 
   
     // console.log(parseFloat(request.body.address)); // validate data
     // if (isNaN(parseFloat(request.body.address))) // check number
