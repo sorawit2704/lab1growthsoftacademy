5b1518299330a96c187d7a585c6ef8ba037bd94c
sorawit2704 <61543306+sorawit2704@users.noreply.github.com>
2020-03-12 17:01:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 
 const foods = require("./db");
-const shop = require("./db");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
